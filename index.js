8dfaaa407d3cbb844c69a31fdbca9646eceeb003
Manuel Celis <manuelcelisforero@gmail.com>
2021-05-11 13:48:53 -0500

    Se agregó la libreria CORS al proyecto

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
